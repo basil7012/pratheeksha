18fddaada9f3aa14946ce015b533a2957ea2eb2a
basil7012 <94462622+basil7012@users.noreply.github.com>
2021-12-05 21:47:16 +0530

    Update urls.py

--- lottery_app/urls.py
+++ lottery_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.home,name='home'),
     path('details/<int:category_id>/',views.details,name='details'),
     path('add_details/<int:category_id>/',views.add_details,name='add_details'),
     path('search', views.searching, name='search')
-
-
-
-
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
